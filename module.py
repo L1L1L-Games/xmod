5845fcf3a6c16d3f228a746aed31e961b380a982
L1L1L-Games <56414267+L1L1L-Games@users.noreply.github.com>
2020-01-14 13:17:11 -0800

    Create module.py

---
+++ module.py
@@ -0,0 +1,28 @@
+import time
+def outlog(a):
+  print(a)
+
+def set(before,after):
+    before = after
+
+def alter(a,b):
+    a = a + b
+
+def usleep(seconds):
+    seconds = seconds/1000
+    time.sleep(seconds)
+
+def grow(plant):
+    plant += 1
+
+def __C__():
+    return "|*****|"
+
+def spaces():
+    return " " * 25
+
+def fcheck(a):
+    if a == True:
+        return True
+    if a == False:
+        return False
